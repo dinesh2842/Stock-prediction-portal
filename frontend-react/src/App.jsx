f99d00946b6fb29d06163e2933daf29888522c5e
dinesh <dineshtech0208@gmail.com>
2025-06-30 17:16:59 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,7 +1,13 @@
 import './assets/css/style.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
+
+
 import Main from './components/Main'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Register from './components/Register'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Login from './components/Login'
+
 
 
 function App() {
@@ -9,9 +15,15 @@ function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
     <nav className='navbar container pt-3 pb-3 align-items-start '>
-            <a href="navbar-brand text-light">Stock Prediction Portal</a>
+            <Link className="navbar-brand text-light" to='/' >Stock Prediction Portal</Link>
             <div>
-                <Button text='Login' class="btn-outline-info"/>
+                <Button text='Login' class="btn-outline-info" url='/login'/>
                 &nbsp;
-                <Button text='Register' class="btn-info" />
+                <Button text='Register' class="btn-info" url='/register' />
 
             </div>
     </nav>
